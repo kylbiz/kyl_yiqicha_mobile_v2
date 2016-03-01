7487674bd6179d7316cddf78138ccf088a77abf1
AircityScript <842736610@qq.com>
2016-03-01 13:41:20 +0800

    register-valid

--- client/accounts/login.js
+++ client/accounts/login.js
@@ -1,27 +1,31 @@
 Template.login.onRendered(function(){
     
-$('form').form('reset');
+$('.ui.form').form('reset');
   
-$.fn.form.settings.rules.isTelephone = function (value) {
-  var pattern = /^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/;
-  return pattern.test(value);
-} 
+$.fn.form.settings.rules.isPhone = function (value) {
+  var pattern = /^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/; 
+  return pattern.test(value); 
+}
+
 $.fn.form.settings.rules.isMatchAccount = function (value) {
   //call
   //...return
   return true;
 } 
 
+
+
 $('.ui.form')
   .form({
     inline : true,  
     onSuccess : function(event, fields) {
-       console.log(fields);
+      console.log(fields);
+      console.log(fields.password);
       //do something...
       alert("success");
     },
     onFailure : function(formErrors, fields) {
-  
+      
     },  
     fields: {
       phone   : {
@@ -31,7 +35,8 @@ $('.ui.form')
             prompt : '请输入手机号'
           },
           {
-            type   : 'isTelephone',
+//            type:'regExp[/^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/]',
+            type:'isPhone',
             prompt : '请输入正确的手机号'            
           }                    
         ]        
---
+++ client/accounts/register.js
@@ -0,0 +1,89 @@
+Template.register.onCreated(function(){
+  Session.set('codeTime', 0);
+});
+
+Template.register.helpers({
+  'codeTime': function(){
+   return Session.get('codeTime');
+  }
+});
+
+Template.register.events({
+  "click #send":function(){    
+		Session.set('codeTime', 60);     
+  }
+})
+
+Template.register.onRendered(function(){
+
+$('.ui.form').form('reset');
+  
+setInterval(function() {
+  if (Session.get('codeTime') > 0) {
+    Session.set('codeTime', Session.get('codeTime') - 1);
+  }
+  else {
+    clearInterval();  
+  }
+}, 1000); 
+  
+$.fn.form.settings.rules.isPhone = function (value) {
+  var pattern = /^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/; 
+  return pattern.test(value); 
+}
+
+$.fn.form.settings.rules.isValidCode = function (value) {
+  //call
+  //...return
+  return true;
+} 
+
+$('.ui.form')
+  .form({
+    inline: true,
+    onSuccess: function (event, fields) {
+      console.log(fields);
+    },
+    onFailure: function (formErrors, fields) {
+
+    },
+    fields: {
+      phone: {
+        rules: [
+          {
+            type: 'empty',
+            prompt: '请输入手机号'
+          },
+          {
+//            type: 'regExp[/^(13[0-9]|14[57]|15[012356789]|17[0678]|18[0-9])\d{8}$/]',
+            type:'isPhone',
+            prompt: '请输入正确的手机号'
+          }
+        ]
+      },
+      password: {
+        rules: [
+          {
+            type: 'empty',
+            prompt: '请输入密码'
+          }
+        ]
+      },
+      verifycode: {
+        rules: [
+          {
+            type:'empty',
+            prompt:'请输入验证码'
+          },
+          {
+            type:'isValidCode',
+            prompt:'验证码不正确'
+          }
+        ] 
+      }
+    }
+  });
+  
+})
+
+               
